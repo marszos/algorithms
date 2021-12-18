06c0b4a3391610c178c31a86f26c4e2c585ce78f
Marcin Szoska <93832363+marszos@users.noreply.github.com>
2021-12-17 19:04:43 -0500

    Added file

---
+++ Binary_search_algorithm.py
@@ -0,0 +1,16 @@
+def binary_search(arr,l,r,x):
+    if l <= r:
+        mid = l + (r - l) // 2
+        if arr[mid] == x:
+            return mid 
+        elif arr[mid] > x:
+            return binary_search(arr,l,mid-1,x)
+        else:
+            return binary_search(arr,mid+1,r,x)
+    else:
+        return 0 
+      
+arr = [2, 3, 4, 10, 40]
+x = 10
+
+binary_search(arr, 0, len(arr)-1, x)
